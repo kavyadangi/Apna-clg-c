083a865833fadd74646070fb22fe980ef3a55dbe
Kavya <kavyadangi09@gmail.com>
2023-02-07 15:48:12 +0530

    Create gnrl.c

---
+++ gnrl.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int i=4;
+  switch(i){
+    default:
+    printf("\n i study in upes");
+    case 1:
+    printf("\n blah blah");
+    break;
+    case 4:
+    printf("\n  blah");
+    break;
+  }
+  return 0;
+}
